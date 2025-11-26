dfdd417eebbeb9a8b3a7f8b24e103a8875a85760
simonjonah <simonudo74@gmail.com>
2025-11-26 18:10:43 +0000

    add repos

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2025 <a href="#">Cherry</a>.</strong>
+    <strong>Copyright &copy; 2025 <a href="https://github.com/Simonjonah">Simon Udo</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.5
